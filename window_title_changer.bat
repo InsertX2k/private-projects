6d609cdf918dbf0ad9cc376081a25c85c14b5535
InsertPlayZ <62176660+ziad-ahmed-said@users.noreply.github.com>
2020-03-28 17:05:44 +0200

    window-title-changer-source-code.bat
    Contains the source code of the project "Window title changer" with their explanation , I'm sorry if you didn't understand anything written in the source code, but you should be a developer to access the source code of the project.
    This source code is free to download, even to modify , and to re-distribute.
    Created by me -InsertPlayZ , PLEASE NOTE THAT THIS SOURCE CODE WON'T WORK ON NORMAL COMMAND PROMPT WINDOW, IT REQUIRES BAT_TO_EXE_CONVERTER BY FATIH KODAK WITH EXTD EXTENSION IN ORDER TO WORK WITH THE SOURCE CODE.
    You can download the compiled version of it from this repository or branch.

--- window_title_changer.bat
+++ window_title_changer.bat
@@ -1,3 +1,5 @@
+#Bat_To_Exe_Converter configuration settings (Do not edit them).
+
 ::[Bat To Exe Converter]
 ::
 ::YAwzoRdxOk+EWAnk
@@ -31,13 +33,29 @@
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
+
+#Disables echoing the input of the interpreter
 @echo off
+
+#Clears the command line screen
 cls
+
+#Changes the title of the window.
 title Window title changer for Windows IA-32 only!
+
+#Changes the color of the console output into f1 
 color f1
+
+#modifies the mode of CON in cmd.exe (sets some values)
 mode con:cols=60 lines=30
+
+#main is a group of batch commands
 :main
+
+#Shows you the warning/disclaimer message as an error message box.
 %extd% /messagebox "Disclaimer" "The current version of Window title changer for Windows is not really that stable, so yeah, if you really wish to continue using it, you totally agree, that we are not responsable to any damages that might occur on your system due to incorrect usage of this project, although we don't give you any kind of warranties that this app will work for you at all, by clicking OK you completely agree that this might happen to your computer.." 16
+
+#Sends some messages into the output of the command line
 echo Welcome to Window title changer for windows ia-32!, so if you just wish
 echo to change the title of any specific window, please just by the simplicity write it's
 echo current name, then we will ask you for the new name of it, when ur ready for that.
@@ -49,18 +67,47 @@ echo ____________________________________________________
 echo Please write the current window title that you wish to change : 
 echo _____
 echo For an example : Calculator, Control Panel,My Computer, This PC,etc..
+
+#Sets a temporary value from the user input (any kind of text input)
 set /p ewin="Current Window title is > "
+
+#Clears the command line screen again.
 cls
+
+#Sends some messages into the output of the command line again.
 echo Right now, you must have to write the window title that you wish to change to.
 echo ______
 echo For an example : My window, you have got hacked, elevation is required.
 echo ______
+
+#Sets a temporary value from the user input again.
 set /p nwin="New Window title is > "
+
+#Clears the command line screen again for the third time.
 cls
+
+#Notifies the user through outputing a word on the command line screen.
 echo Please wait while we apply your changes...
+
+#Uses the previous and the current values that the user has just written to make changes to the window title.
 %extd% /setwindowtitle "%ewin%" "%nwin%"
+#%ewin% - Contains the current window title without any changes (from the user)
+#%nwin% - Contains the new window title after modification (from the user and %extd% option)
+
+#Clears the command line screen again for the fourth time.
 cls
+
+#Notifies the user that the operation has been successfully completed!
 echo Done, we did everything you just wish ....
+
+#Waits for an input from keyboard by the user (Press any key to continue...)
 pause
+
+#Clears the command line screen and get's it ready to repeat everything again.
 cls
+
+#Repeat everything again - through goto command in cmd.exe
 goto main
+
+#If you can't download the source code , or it just have got downloaded but with those #s , you must remove them before using this file in the Bat_To_Exe_Converter by Fatih Kodak
+#You can download the source code for free, and even modify it and share it as you like (But under GNU GPL +2 only!)
